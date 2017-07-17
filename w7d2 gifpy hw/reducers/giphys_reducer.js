7fc260c0b60c74b95954f7543789cc2b3f9fa3cf
Janet Lee <lee.cjanet@gmail.com>
2017-07-16 18:39:35 -0700

    w4d3 auth hw

--- w7d2 gifpy hw/reducers/giphys_reducer.js
+++ w7d2 gifpy hw/reducers/giphys_reducer.js
@@ -1,1 +1,15 @@
 import { RECEIVE_SEARCH_GIPHYS } from '../actions/giphy_actions';
+
+let initialState = [];
+
+const GiphyReducer(previous_state=initialState, action) => {
+
+  switch (action.type) {
+    case RECEIVE_SEARCH_GIPHYS:
+      return action.giphys;
+    default:
+      return previous_state;
+  }
+};
+
+export default GiphysReducer;
--- w7d2 gifpy hw/store/store.js
+++ w7d2 gifpy hw/store/store.js
@@ -1,3 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import RootReducer from '../reducers/root_reducer';
+
+// import RootReducers from '../reducers/root_reducer'
+
+
+const configureStore = () => {
+  return createStore(RootReducer);
+};
+
+export default configureStore;
